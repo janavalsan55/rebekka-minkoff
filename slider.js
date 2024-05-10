3466f2fd811ae8238e2528ce70d339d322711eea
janavalsan55 <janavalsan@gmail.com>
2024-05-10 17:13:34 +0530

    finalised

--- slider.js
+++ slider.js
@@ -4,4 +4,56 @@ $(document).ready(function(){
       slidesToScroll: 1,
       infinite:false,
     });
+
+
+    $('.link1').mouseover(function () {
+      $('.dp1').slideDown("slow");
+    })
+    $('.link1').mouseleave(function () {
+      $('.dp1').slideUp("");
+    })
+    $('.link2').mouseover(function () {
+      $('.dp2').slideDown("slow");
+    })
+    $('.link2').mouseleave(function () {
+      $('.dp2').slideUp("slow");
+    })
+    $('.link3').mouseover(function () {
+      $('.dp3').slideDown("slow");
+    })
+    $('.link3').mouseleave(function () {
+      $('.dp3').slideUp();
+    })
+    $('.link4').mouseover(function () {
+      $('.dp4').slideDown("slow");
+    })
+    $('.link4').mouseleave(function () {
+      $('.dp4').slideUp();
+    })
+    $('.link5').mouseover(function () {
+      $('.dp5').slideDown("slow");
+    })
+    $('.link5').mouseleave(function () {
+      $('.dp5').slideUp();
+    })
+    $('.currency').click(function () {
+      $('.dorpdowncurr').slideToggle("");
+    })
+    $('.user').click(function () {
+      $('.dropdownuser').slideToggle("");
+    })
+    $('.create').click(function () {
+      $('.userformlogin').hide("");
+      $('.userformsignup').show("");
+    })
+    $('.loginbt').click(function () {
+      $('.userformsignup').hide("");
+      $('.userformlogin').show("");
+    })
+$(".menuitem").click(function () {
+  var div = $('.drop');
+  div.animate({  left: div.css('left') === '0px' ? '-450px' : '0px'
+}, "");
+});
 })
+
